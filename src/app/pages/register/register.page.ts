25522129a15b8c973961507dc10469bee9a18687
devtechkamal <131899275+devtechkamal@users.noreply.github.com>
2025-08-17 16:21:47 +0530

    add ui changes
    add ui changes

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,5 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { SharedModule } from '../../shared/shared-module';
 import { RouterLink } from '@angular/router';
 
