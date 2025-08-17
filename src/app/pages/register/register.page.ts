2b63e7e41f62d76ca61da6dc7bfeba67ddc00292
devtechkamal <131899275+devtechkamal@users.noreply.github.com>
2025-08-17 16:16:49 +0530

    add ui changes
    add ui changes

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -11,22 +11,4 @@ import { RouterLink } from '@angular/router';
   styleUrl: './register.page.css'
 })
 export class RegisterPage {
-  fb: FormBuilder = inject(FormBuilder);
-  registerForm: FormGroup = this.fb.group({
-    name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required])
-  });
-
-  registerFormCtrl(field: string): FormControl {
-    return this.registerForm.get(field) as FormControl;
-  }
-
-  onSubmit() {
-    if (this.registerForm.valid) {
-      console.log(this.registerForm.value);
-    } else {
-      this.registerForm.markAllAsTouched();
-    }
-  }
 }
